cb027e4648126b9b3d7be293535866295d9ca102
Vira Murray <vira.avramenko@gmail.com>
2022-10-26 14:36:45 -0400

    Add project files.

---
+++ Home-Owners-Association-MVC-APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Home_Owners_Association_MVC_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
