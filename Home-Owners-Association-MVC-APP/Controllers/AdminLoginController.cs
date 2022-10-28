6c6b3abbcdc5a3156b045e671beba190f1c6854a
Vira Murray <vira.avramenko@gmail.com>
2022-10-28 12:01:22 -0400

    added links for AdminLogin and OwnersLogin Views

---
+++ Home-Owners-Association-MVC-APP/Controllers/AdminLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Home_Owners_Association_MVC_APP.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+
+        private readonly IRequestRepository repo;
+
+        public AdminLoginController(IRequestRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            var requests = repo.GetAllRequests();
+            return View(requests);
+        }
+    }
+}
---
+++ Home-Owners-Association-MVC-APP/Controllers/OwnersLogin.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Home_Owners_Association_MVC_APP.Controllers
+{
+    public class OwnersLogin : Controller
+    {
+        private readonly IRequestRepository repo;
+
+        public OwnersLogin(IRequestRepository repo)
+        {
+            this.repo = repo;
+        }
+
+
+        public IActionResult Index()
+        {
+            var requests = repo.GetAllRequests();
+            return View(requests);
+        }
+    }
+}
